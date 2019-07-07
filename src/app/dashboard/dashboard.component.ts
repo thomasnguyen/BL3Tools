c21a450fa09deaaa23f5fd40365dfeb727bd2828
Thomas Nguyen <imtoshiru@gmail.com>
2019-07-07 16:12:01 -0700

    Made dashboard page mobile friendly"

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { CharSelectComponent } from '../char-select/char-select.component';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  charScreenRef: BsModalRef;
 
-  constructor() { }
+  constructor(
+    private modalService: BsModalService,
+  ) { }
 
   ngOnInit() {
+  }
+
+  openCharSelect() {
+    const initialState = {
+      title: 'Choose your character',
+      class: 'gray modal-lg'
+    };
+    this.charScreenRef = this.modalService.show(CharSelectComponent, { initialState });
   }
 
 }
