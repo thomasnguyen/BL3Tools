3e5d40106b9053cd890f19ae16c57548748f2b14
Thomas Nguyen <imtoshiru@gmail.com>
2019-08-04 21:52:14 -0700

    Imported and finsihed tree #2 for zane

--- src/app/services/zane-skills.service.ts
+++ src/app/services/zane-skills.service.ts
@@ -328,11 +328,11 @@ export class ZaneSkillsService {
       description: `Whenever one or more of Zane's Action Skills are active, he gains increased Gun Damage for each active Action Skill.	`,
       type: 'skill',
       skillLimit: 5,
-      position: [0, 2],
+      position: [1, 1],
       skillCount: 0,
 
       getEffect: (rank) => `
-      2% Gun Damage per active Action Skill
+       ${rank * 2}% Gun Damage per active Action Skill
       `,
 
       getDescription: function () {
@@ -345,11 +345,11 @@ export class ZaneSkillsService {
       description: `Zane and his Digi-Clone gain increased Magazine Size.`,
       type: 'skill',
       skillLimit: 3,
-      position: [0, 2],
+      position: [1, 2],
       skillCount: 0,
 
       getEffect: (rank) => `
-      4% Magazine Size
+      ${rank * 4}% Magazine Size
       `,
 
       getDescription: function () {
@@ -360,13 +360,13 @@ export class ZaneSkillsService {
       name: 'Borrowed Time',
       image: '',
       description: `Zane gains increased Action Skill Duration for every active Action Skill.`,
-      type: 'action-skill',
+      type: 'skill',
       skillLimit: 5,
-      position: [0, 2],
+      position: [1, 3],
       skillCount: 0,
 
       getEffect: (rank) => `
-      4% Action Skill DUration per active Action Skill
+      ${rank * 4}% Action Skill DUration per active Action Skill
       `,
 
       getDescription: function () {
@@ -379,7 +379,7 @@ export class ZaneSkillsService {
       description: `Whenever Zane swaps places with his Clone, a Cryo Nova is triggered around Zane and his Clone.`,
       type: 'augment',
       skillLimit: 1,
-      position: [0, 2],
+      position: [2, 0],
       skillCount: 0,
 
       getEffect: (rank) => `
@@ -397,11 +397,11 @@ export class ZaneSkillsService {
       he and his Digi-Clonee receive increased Gun Damage and gain Health Regeneration for a few seconds.`,
       type: 'skill',
       skillLimit: 5,
-      position: [0, 2],
+      position: [2, 1],
       skillCount: 0,
 
       getEffect: (rank) => `
-      2% Gun Damage, 0.5% Health Regeneration per second related to Missing Health, 8 second Duration
+      ${rank * 2}% Gun Damage, ${rank * 0.5}% Health Regeneration per second related to Missing Health, 8 second Duration
 
       `,
 
@@ -416,8 +416,8 @@ export class ZaneSkillsService {
       If the Digi-Clone is killed, it drops a free grenade.
       Kill Skill. Killing an enemy while the Digi-Clone is active gives the Clone a chance to throw a grenade.`,
       type: 'skill',
-      skillLimit: 5,
-      position: [0, 2],
+      skillLimit: 1,
+      position: [2, 2],
       skillCount: 0,
 
       getEffect: (rank) => `
@@ -435,11 +435,11 @@ export class ZaneSkillsService {
       The more grenades in his capacity, the higher the chance.`,
       type: 'skill',
       skillLimit: 5,
-      position: [0, 2],
+      position: [2, 3],
       skillCount: 0,
 
       getEffect: (rank) => `
-      Up to 5% Grenade Chance
+      Up to ${rank * 5}% Grenade Chance
       `,
 
       getDescription: function () {
@@ -451,7 +451,7 @@ export class ZaneSkillsService {
       description: `Whenever the Clone takes damage, Zane's shield is restored by a portion of that damage.`,
       type: 'augment',
       skillLimit: 1,
-      position: [0, 2],
+      position: [3, 0],
       skillCount: 0,
 
       getEffect: (rank) => `
@@ -468,7 +468,7 @@ export class ZaneSkillsService {
       description: `Whenever Zane swaps places with his Clone, his shield immediately begins recharging.`,
       type: 'skill',
       skillLimit: 1,
-      position: [0, 2],
+      position: [3, 2],
       skillCount: 0,
 
       getEffect: (rank) => `
@@ -484,7 +484,7 @@ export class ZaneSkillsService {
       description: `Enemies are more likely to target the Clone for a few seconds after it's summoned and after swapping places.`,
       type: 'augment',
       skillLimit: 1,
-      position: [0, 2],
+      position: [3, 4],
       skillCount: 0,
 
       getEffect: (rank) => `
@@ -503,7 +503,7 @@ export class ZaneSkillsService {
        The more Action Skill time remaining, the greater the damage.`,
       type: 'augment',
       skillLimit: 1,
-      position: [0, 2],
+      position: [4, 0],
       skillCount: 0,
 
       getEffect: (rank) => `
@@ -520,11 +520,11 @@ export class ZaneSkillsService {
       description: `Kill Skill. After killing an enemy, Zane gains Grenade Regeneration for a few seconds.	`,
       type: 'skill',
       skillLimit: 3,
-      position: [0, 2],
+      position: [4, 1],
       skillCount: 0,
 
       getEffect: (rank) => `
-      5% Grenade Regeneration, 8 second Duration
+      ${rank * 5}% Grenade Regeneration, 8 second Duration
       `,
 
       getDescription: function () {
@@ -537,8 +537,8 @@ export class ZaneSkillsService {
       description: `Press the Action Skill button during Fight for Your Life if Digi-Clone is active to
       destroy the clone and immediately gain a Second Wind with full health.`,
       type: 'skill',
-      skillLimit: 3,
-      position: [0, 2],
+      skillLimit: 1,
+      position: [4, 2],
       skillCount: 0,
 
       getEffect: (rank) => `
@@ -555,11 +555,11 @@ export class ZaneSkillsService {
       he gains increased Movement Speed for each active Action Skill.`,
       type: 'skill',
       skillLimit: 3,
-      position: [0, 2],
+      position: [4, 3],
       skillCount: 0,
 
       getEffect: (rank) => `
-      4% Movement Speed per active Action Skill
+      ${rank * 4}% Movement Speed per active Action Skill
       `,
 
       getDescription: function () {
@@ -570,9 +570,9 @@ export class ZaneSkillsService {
       name: `Digital Distribution`,
       image: '',
       description: `If Zane takes health damage while the Clone is active, a portion of that damage is shared to his Clone instead.`,
-      type: 'skill',
+      type: 'augment',
       skillLimit: 3,
-      position: [0, 2],
+      position: [4, 4],
       skillCount: 0,
 
       getEffect: (rank) => `
@@ -586,14 +586,15 @@ export class ZaneSkillsService {
     {
       name: `Like a Ghost`,
       image: '',
-      description: `Zane and his Digi-Clone gain a chance to ignore bullets. This chance is increased for a few seconds after activating an action skill. This effect stacks.`,
+      description: `Zane and his Digi-Clone gain a chance to ignore bullets.
+       This chance is increased for a few seconds after activating an action skill. This effect stacks.`,
       type: 'skill',
       skillLimit: 3,
-      position: [0, 2],
+      position: [5, 1],
       skillCount: 0,
 
       getEffect: (rank) => `
-      4% Ignore Bullet Chance, 1% Additional Ignore Bullet Chance, 8 second Duration
+      ${rank * 4}% Ignore Bullet Chance, ${rank * 1}% Additional Ignore Bullet Chance, 8 second Duration
       `,
 
       getDescription: function () {
@@ -606,8 +607,8 @@ export class ZaneSkillsService {
       description: `Whenever Zane summons his Digi-Clone, it consumes up to 3 grenades.
       For every grenade consumed, the Digi-Clone gains increased Gun Damage, Max Health, Fire Rate and Reload Speed.`,
       type: 'skill',
-      skillLimit: 3,
-      position: [0, 2],
+      skillLimit: 1,
+      position: [5, 2],
       skillCount: 0,
 
       getEffect: (rank) => `
@@ -624,11 +625,11 @@ export class ZaneSkillsService {
       description: `Zane deals Bonus Shock Damage to enemies that aren't targeting him.`,
       type: 'skill',
       skillLimit: 3,
-      position: [0, 2],
+      position: [5, 3],
       skillCount: 0,
 
       getEffect: (rank) => `
-      6% of damage dealt as Bonus Damage
+      ${rank * 6}% of damage dealt as Bonus Damage
       `,
 
       getDescription: function () {
@@ -641,8 +642,8 @@ export class ZaneSkillsService {
       description: `The Clone is equipped with a copy of Zane's Current Weapon when activated.
       Swapping places with the Clone causes Zane and his clone to gain increased Gun Damage.`,
       type: 'skill',
-      skillLimit: 3,
-      position: [0, 2],
+      skillLimit: 1,
+      position: [6, 2],
       skillCount: 0,
 
       getEffect: (rank) => `
